9f49ba2ffe110832e0154d262e952c236a3d5a91
Fer Kova <ferkovalink@gmail.com>
2022-04-04 00:19:13 -0300

    test

---
+++ AppCoder/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+def curso(self):
+    curso = Curso(nombre= 'Desarrollo Web', camada= '1111')
+
+    curso.save()
+
+    documento = f'El curso es: {curso.nombre}, la camada: {curso.camada}'
+
+    return HttpResponse(documento)
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
+
+
+
+
+
+
+
+
+
+
